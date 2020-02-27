0d8243c5c98429613f22486548f19f425b95b0c7
Azee <aziztitu1996@gmail.com>
2020-02-26 20:35:19 -0600

    Shows word count in demo

--- js/app.js
+++ js/app.js
@@ -5,61 +5,74 @@ var matchesDiv;
 var readyStatus;
 
 function main() {
-  wordsTextArea  = $("#words");
-  patternInput = $("#pattern");
-  matchesDiv = $("#matches");
-  readyStatus = $("#readyStatus");
+    wordsTextArea = $('#words');
+    patternInput = $('#pattern');
+    matchesDiv = $('#matches');
+    readyStatus = $('#readyStatus');
 
-  readyStatus.hide();
+    readyStatus.hide();
 
-  // var testWords = ["Azee", "Mississipi", "zee", "Banana", "nana"];
+    // var testWords = ["Azee", "Mississipi", "zee", "Banana", "nana"];
 
-  // testWords.forEach(word => {
-  //   suffixTree.addString(word);
-  // });
+    // testWords.forEach(word => {
+    //   suffixTree.addString(word);
+    // });
 
-  //console.dir(suffixTree);
-  //suffixTree.print();
+    //console.dir(suffixTree);
+    //suffixTree.print();
 }
 
 function preprocess() {
-  var words = wordsTextArea.val().split(/(\s+)/);
-  console.log(wordsTextArea.val());
-  console.log(words);
+    var words = wordsTextArea.val().split(/(\s+)/);
+    console.log(wordsTextArea.val());
+    console.log(words);
 
-  words.forEach(word => {
-    suffixTree.addString(word);
-  });
+    words.forEach((word) => {
+        suffixTree.addString(word);
+    });
 
-  wordsTextArea.val("");
+    wordsTextArea.val('');
 
-  readyStatus.show();
+    readyStatus.show();
 
-  console.dir(suffixTree);
+    console.dir(suffixTree);
 
-  search();
+    search();
 }
 
 function search() {
-  var pattern = patternInput.val();
-  console.log("Pattern: " + pattern);
+    var pattern = patternInput.val();
+    console.log('Pattern: ' + pattern);
 
-  var matches = suffixTree.search(pattern);
+    var matches = suffixTree.search(pattern);
 
-  console.log(matches);
+    console.log(matches);
+
+    var matchesHash = {};
+    matches.forEach((match) => {
+        if (match in matchesHash) {
+            matchesHash[match]++;
+        } else {
+            matchesHash[match] = 1;
+        }
+    });
+
+    console.log(matchesHash);
 
 
-  var matchesStr = "";
-  matches.forEach(match => {
-    matchesStr += match + "<br/>";
-  });
-  matchesDiv.html(matchesStr);
+    var matchesStr = '';
+    for (var match in matchesHash) {
+      matchesStr += match + (matchesHash[match] > 1 ? " (x" + matchesHash[match] + ")" : "");
+      matchesStr += '<br/>';
+    }
+
+    matchesDiv.html(matchesStr);
 }
 
 function reset() {
-  wordsTextArea.val("");
-  patternInput.val("");
-  matchesDiv.html("");
-  suffixTree.clear();
-  readyStatus.hide();
+    wordsTextArea.val('');
+    patternInput.val('');
+    matchesDiv.html('');
+    suffixTree.clear();
+    readyStatus.hide();
 }
